c1ba3cfb2b4371409dfdf3510a7329ed5d5ac01a
Harpo MAxx <harpomaxx@gmail.com>
2022-03-04 12:23:14 -0300

    add boruta stage

--- code/R/scripts/select_features_boruta.R
+++ code/R/scripts/select_features_boruta.R
@@ -1,6 +1,6 @@
-require(Boruta)
-require(dplyr)
-require(tibble)
+suppressPackageStartupMessages(library(Boruta))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(tibble))
 
 #' Feature Selection using Boruta
 #'
@@ -30,8 +30,57 @@ select_features_boruta <- function(train_dataset_boruta,
     attStats(var_importance_boruta_raw) %>%
     filter(decision == 'Confirmed') %>%
     select(meanImp) %>%
-    arrange(desc(meanImp)) %>% top_n(num_of_feat)
+    arrange(desc(meanImp)) %>% top_n(num_of_feat,meanImp)
   var_importance_boruta <- var_importance_boruta %>%
     tibble::rownames_to_column('variable')
   var_importance_boruta
+}
+
+
+#' Select the Max N features observed during all the bootstrap resampling.
+#' Since for selecting a variable, it has to be present Boruta output,
+#' It could be possible to obtain a value less than `num_of_feat`
+#' 
+#' @param dataset 
+#' @param num_of_feat the max number of features to be selected 
+#' @param num_of_samp the number of samples used by bootstrap
+#' @return dataframe with top N features
+#' @export
+#'
+#' @examples
+select_maxn_features <- function(dataset,
+                                 max_num_of_feat = 20,
+                                 num_of_samp = 30){
+  suppressPackageStartupMessages(library(doMC))
+  suppressPackageStartupMessages(library(rsample))
+  registerDoMC(cores = 4)
+  message("[] Selecting best features.",appendLF = FALSE )
+  resamples <-
+    rsample::bootstraps(dataset, strata = Activity, times = num_of_samp)
+  var_importance <-
+    foreach(i = 1:length(resamples$splits), .combine = rbind) %dopar% {
+      trainset <- rsample::analysis(resamples$splits[[i]])
+      partial <- select_features_boruta(trainset, "Activity",max_num_of_feat)
+      partial <- cbind(partial, rsample = i)
+      message(".",appendLF = FALSE)
+      partial
+    }
+  message("\n[] Done.")
+  var_always_present <-
+    var_importance %>% group_by(variable) %>% summarise(n = n()) %>% 
+    filter(n == num_of_samp) %>% ungroup() %>% select(variable)
+  v <- var_always_present %>% unlist() %>% unname()
+  var_importance_boruta_final <-
+    var_importance %>% filter(variable %in% v) %>% group_by(variable) %>% 
+    summarise(grandmeanImp = mean(meanImp)) %>% 
+    arrange(grandmeanImp %>% desc()) %>% ungroup() %>% select(variable)
+  
+  var_importance_boruta_final <- 
+    apply(var_importance_boruta_final$variable %>% t() , 2,
+          function(x)
+            stringr::str_replace_all(x,
+                                     pattern = "`",
+                                     replacement = "")) %>% as.data.frame()
+  names(var_importance_boruta_final) <- "variable"
+  var_importance_boruta_final
 }
---
+++ code/R/scripts/select_features_boruta_dataset.R
@@ -0,0 +1,75 @@
+#!/bin/Rscript
+#  separate train and test datasets
+
+source("code/R/scripts/select_features_boruta.R")
+suppressPackageStartupMessages(library(optparse))
+suppressPackageStartupMessages(library(readr))
+suppressPackageStartupMessages(library(dplyr))
+suppressPackageStartupMessages(library(yaml))
+suppressPackageStartupMessages(library(rjson))
+#### MAIN 
+
+option_list <- list(
+  make_option(
+    "--input",
+    action = "store",
+    type = "character",
+    help = "Set the name of the input data file in tsv format"
+  ),
+  make_option(
+    "--output",
+    action = "store",
+    type = "character",
+    default = "",
+    help = "Set the basename of the output data files in tsv format"
+  ),
+  make_option(
+    "--maxnfeat",
+    action = "store",
+    type = "integer",
+    default = 40,
+    help = "Set the max number of features to select."
+  ),
+  make_option(
+    "--numsamp",
+    action = "store",
+    type = "integer",
+    default = 30,
+    help = "Set the number of samples in boostrap"
+  )
+)
+opt <- parse_args(OptionParser(option_list=option_list))
+
+if (opt$input %>% is.null() ||
+    opt$output %>% is.null()) {
+  message("[] Parameters missing. Please use --help for looking at the available parameters.")
+  quit()
+} else{
+  ## Read dataset
+  dataset <-
+    readr::read_delim(opt$input, col_types = cols(), delim = '\t')
+  dataset$Activity<-as.factor(dataset$Activity)
+  ## Set default parameters
+  maxnfeat <- opt$maxnfeat
+  numsamp <- opt$numsamp
+  params <- yaml::read_yaml("params.yaml")
+  if( "select_features_boruta" %in% names(params)){
+    maxnfeat <- params$select_features_boruta$maxnfeat
+    numsamp <- params$select_features_boruta$numsamp
+  }
+  ## Select features
+  selected_features_dataframe <-
+    select_maxn_features(dataset = dataset, 
+                         max_num_of_feat = maxnfeat,
+                         num_of_samp = numsamp)
+  ## Save dataset
+  dir.create(dirname(opt$output), showWarnings = FALSE)
+  readr::write_delim(selected_features_dataframe, 
+                     file = paste0(opt$output,"_selected_features.tsv"), 
+                     delim = '\t')
+  ## Save Metric
+  list("metrics"=list("number_of_features_selected"=nrow(selected_features_dataframe)))%>% 
+    as.yaml() %>% write("select_features_boruta.yaml",)
+  #list("number_of_features_selected"=nrow(selected_features_dataframe))%>% rjson::toJSON() %>% write("select_features_boruta-nfeat.json")
+  
+} 
