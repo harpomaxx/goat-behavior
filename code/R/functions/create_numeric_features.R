75d67ca835050eebb717a589e6e8fc76b44a3163
Harpo MAxx <harpomaxx@gmail.com>
2022-09-08 12:31:46 -0300

    change animal names

--- code/R/functions/create_numeric_features.R
+++ code/R/functions/create_numeric_features.R
@@ -77,21 +77,21 @@ create_numeric_features <- function(dataset) {
 rename_animals <- function(dataset){
 
   dataset$Anim <- as.character(dataset$Anim)
-  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"1553"), "a1"))
-  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"1635"), "a2"))
-  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"1636"), "a3"))
-  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"1646"), "a4"))
-  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"1677"), "a5"))
-  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"1713"), "a6"))
-  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"1779"), "a7"))
-  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"505001"), "a8"))
-  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"505019"), "a9"))
-  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"505035"), "a10"))
-  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"505048"), "a11"))
-  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"505058"), "a12"))
-  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"505071"), "a13"))
-  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"505075"), "a14"))
-  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"505080"), "a15"))
-  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"505081"), "a16"))
+  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"1553"), "a1"))			#a1
+  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"1635"), "a15"))			#a2
+  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"1636"), "a3"))			#a3
+  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"1646"), "a14"))			#a4
+  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"1677"), "a11"))			#a5
+  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"1713"), "a6"))			#a6
+  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"1779"), "a8"))			#a7
+  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"505001"), "a7"))			#a8
+  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"505019"), "a9"))			#a9
+  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"505035"), "a10"))			#a10
+  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"505048"), "a5"))			#a11
+  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"505058"), "a12"))			#a12
+  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"505071"), "a13"))			#a13
+  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"505075"), "a4"))			#a14
+  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"505080"), "a2"))			#a15
+  dataset<- dataset %>% mutate(Anim = replace(Anim, stringr::str_detect(Anim,"505081"), "a16"))			#a16
   dataset
 }
